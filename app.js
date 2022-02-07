3360178418b5af8d8601e9a080ad0020dac8d4fc
Ahmad Fahrezi <ahmadfahrezi8127@gmail.com>
2022-02-07 20:31:40 +0700

    API key

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const client = contentful.createClient({
 	// This is the space ID. A space is like a project folder in Contentful terms
-	space: process.env.space,
+	space: "3zlo4n83ek4v",
 	// This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-	accessToken: process.env.accessToken,
+	accessToken: "FgsY0QomAeYJAZ0gha2Ps2drfJWnXWLoOCpyh5xjh0Y",
 });
 
 console.log(client);
